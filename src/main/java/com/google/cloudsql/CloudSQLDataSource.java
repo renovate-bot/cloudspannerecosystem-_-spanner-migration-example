708e81aeb83bbcd227a786697d5cd6a0a5a69f30
Thiago Nunes <thiagotnunes@google.com>
2024-01-12 13:55:17 +1100

    feat: better error messages

--- src/main/java/com/google/cloudsql/CloudSQLDataSource.java
+++ src/main/java/com/google/cloudsql/CloudSQLDataSource.java
@@ -9,19 +9,33 @@ public class CloudSQLDataSource {
   // secure - consider a more secure solution such as
   // Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
   // keep secrets safe.
-  private static final String DATABASE_NAME = System.getenv("CLOUDSQL_DATABASE");
-  private static final String INSTANCE_CONNECTION_NAME = System.getenv("CLOUDSQL_INSTANCE_CONNECTION_NAME");
-  private static final String USERNAME = System.getenv("CLOUDSQL_USERNAME");
-  private static final String PASSWORD = System.getenv("CLOUDSQL_PASSWORD");
+  private static final String DATABASE_NAME_VAR = "CLOUDSQL_DATABASE";
+  private static final String INSTANCE_CONNECTION_NAME_VAR = "CLOUDSQL_INSTANCE_CONNECTION_NAME";
+  private static final String USERNAME_VAR = "CLOUDSQL_USERNAME";
+  private static final String PASSWORD_VAR = "CLOUDSQL_PASSWORD";
 
   public static DataSource createConnectionPool() {
+    String databaseName = getEnvVariable(DATABASE_NAME_VAR);
+    String instanceConnectionName = getEnvVariable(INSTANCE_CONNECTION_NAME_VAR);
+    String username = getEnvVariable(USERNAME_VAR);
+    String password = getEnvVariable(PASSWORD_VAR);
+
     HikariConfig config = new HikariConfig();
-    config.setJdbcUrl(String.format("jdbc:postgresql:///%s", DATABASE_NAME));
-    config.setUsername(USERNAME);
-    config.setPassword(PASSWORD);
+    config.setJdbcUrl(String.format("jdbc:postgresql:///%s", databaseName));
+    config.setUsername(username);
+    config.setPassword(password);
     config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory");
-    config.addDataSourceProperty("cloudSqlInstance", INSTANCE_CONNECTION_NAME);
+    config.addDataSourceProperty("cloudSqlInstance", instanceConnectionName);
 
     return new HikariDataSource(config);
+  }
+
+  private static String getEnvVariable(String name) {
+    String value = System.getenv(name);
+    if (value == null) {
+      System.err.println("Error: Please set the environment variable " + name);
+      System.exit(1);
+    }
+    return value;
   }
 }
--- src/main/java/com/google/spanner/SpannerDataSource.java
+++ src/main/java/com/google/spanner/SpannerDataSource.java
@@ -5,18 +5,28 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 
 public class SpannerDataSource {
-  private static final String DATABASE_NAME = System.getenv("SPANNER_DATABASE");
+  private static final String DATABASE_NAME_VAR = "SPANNER_DATABASE";
   public static DataSource createConnectionPool() {
     try {
       // Make sure the PG JDBC driver is loaded.
       Class.forName("org.postgresql.Driver");
 
+      String databaseName = getEnvVariable(DATABASE_NAME_VAR);
       HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(String.format("jdbc:postgresql://localhost:5432/%s", DATABASE_NAME));
+      config.setJdbcUrl(String.format("jdbc:postgresql://localhost:5432/%s", databaseName));
 
       return new HikariDataSource(config);
     } catch (ClassNotFoundException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private static String getEnvVariable(String name) {
+    String value = System.getenv(name);
+    if (value == null) {
+      System.err.println("Error: Please set the environment variable " + name);
+      System.exit(1);
+    }
+    return value;
   }
 }
