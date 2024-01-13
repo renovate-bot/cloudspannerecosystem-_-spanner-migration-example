3c7a782dbab4148639aadcb00d5b6f703833522e
Thiago Nunes <thiagotnunes@google.com>
2024-01-13 12:42:09 +1100

    feat: adds spanner container

--- src/main/java/com/google/spanner/SpannerDataSource.java
+++ src/main/java/com/google/spanner/SpannerDataSource.java
@@ -5,15 +5,22 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 
 public class SpannerDataSource {
+
+  private static final String PGADAPTER_HOST_VAR = "PGADAPTER_HOST";
+  private static final String PGADAPTER_PORT_VAR = "PGADAPTER_PORT";
   private static final String DATABASE_NAME_VAR = "SPANNER_DATABASE";
+
   public static DataSource createConnectionPool() {
     try {
       // Make sure the PG JDBC driver is loaded.
       Class.forName("org.postgresql.Driver");
 
       String databaseName = getEnvVariable(DATABASE_NAME_VAR);
+      String pgAdapterHost = getEnvVariable(PGADAPTER_HOST_VAR);
+      int pgAdapterPort = Integer.parseInt(getEnvVariable(PGADAPTER_PORT_VAR));
       HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(String.format("jdbc:postgresql://localhost:5432/%s", databaseName));
+      config.setJdbcUrl(
+          String.format("jdbc:postgresql://%s:%d/%s", pgAdapterHost, pgAdapterPort, databaseName));
 
       return new HikariDataSource(config);
     } catch (ClassNotFoundException e) {
