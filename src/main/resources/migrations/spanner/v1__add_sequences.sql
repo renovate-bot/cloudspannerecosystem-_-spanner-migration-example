1cf7d6beba6855739d00951cadcab56dc38a683a
Thiago Nunes <thiagotnunes@google.com>
2024-01-30 11:46:12 +1100

    fix: updates spanner migrations

--- src/main/resources/migrations/spanner/v1__add_sequences.sql
+++ src/main/resources/migrations/spanner/v1__add_sequences.sql
@@ -3,10 +3,10 @@
 -- conflict with existing SERIAL values from the PostgreSQL database.
 
 CREATE SEQUENCE singer_id_seq BIT_REVERSED_POSITIVE;
-ALTER TABLE singers ALTER COLUMN singer_id SET DEFAULT nextval('singer_id_seq'::text);
+ALTER TABLE singers ALTER COLUMN singer_id SET DEFAULT nextval('singer_id_seq');
 
 CREATE SEQUENCE album_id_seq BIT_REVERSED_POSITIVE;
-ALTER TABLE albums ALTER COLUMN album_id SET DEFAULT nextval('album_id_seq'::text);
+ALTER TABLE albums ALTER COLUMN album_id SET DEFAULT nextval('album_id_seq');
 
 CREATE SEQUENCE song_id_seq BIT_REVERSED_POSITIVE;
-ALTER TABLE songs ALTER COLUMN song_id SET DEFAULT nextval('song_id_seq'::text);
+ALTER TABLE songs ALTER COLUMN song_id SET DEFAULT nextval('song_id_seq');
